3b69564db14b38c6113f09c064b090159cb128b4
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2020-09-26 10:13:34 +0800

    TitleSeparator

---
+++ src/components/examples/atoms/TitleSeparator.vue
@@ -0,0 +1,21 @@
+ <template>
+  <div class="example pa-md" style="width: 400px">
+    <div class="text-h6 text-white pb-md">Example on a dark background</div>
+    <TitleSeparator> Example Title </TitleSeparator>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+// $
+
+.example
+  background-color: black
+</style>
+
+<script>
+import TitleSeparator from 'components/atoms/TitleSeparator.vue'
+
+export default {
+  components: { TitleSeparator },
+}
+</script>
